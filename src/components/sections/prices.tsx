64a1e372867aafa534aacb88f133c30005453447
Enmanuel <14-05775@usb.ve>
2024-09-26 21:09:11 -0400

    fix

--- src/components/sections/prices.tsx
+++ src/components/sections/prices.tsx
@@ -1,12 +1,3 @@
-
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from "@/components/ui/dialog"
 import Link from "next/link"
 import { Checkbox } from "@/components/ui/checkbox"
 
